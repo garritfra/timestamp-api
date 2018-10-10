c5aa29a0a91a8077f3f36cb38a069929c2cfef86
Glitch (dear-beet) <none>
2018-10-10 21:52:01 +0200

    Add default time to /

--- server.js
+++ server.js
@@ -2,66 +2,55 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 var filterInt = function(value) {
-  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
-    return Number(value);
+  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return Number(value);
   return NaN;
-}
-
-
-
+};
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.redirect("/api/timestamp/1")
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.redirect("/api/timestamp/" + new Date().getTime());
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
 app.get("/api/timestamp/:timestamp", (req, res) => {
   const input = req.params.timestamp;
   const inputInt = filterInt(input);
   let date = null;
-  
-  if(isNaN(inputInt)) {
+
+  if (isNaN(inputInt)) {
     date = new Date(input);
   } else {
-    date = new Date(inputInt)  
+    date = new Date(inputInt);
   }
-  
-  
+
   let naturalDateStr;
   try {
-      naturalDateStr = date.toUTCString();
-  } catch(e) {
-    naturalDateStr = "Not a valid date"
+    naturalDateStr = date.toUTCString();
+  } catch (e) {
+    naturalDateStr = "Not a valid date";
   }
 
-  
-  res.json({ "unix": date.getTime(), "natural": naturalDateStr });
-})
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  res.json({ unix: date.getTime(), natural: naturalDateStr });
 });
 
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
